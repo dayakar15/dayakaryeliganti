b858d40efa75e9c67b335b4dfb9a1409d27c21aa
Yeliganti Dayakar <ydayakar94@gmail.com>
2026-08-31 19:54:57 +0530

    Update projects-preview.tsx

--- components/home/projects-preview.tsx
+++ components/home/projects-preview.tsx
@@ -13,7 +13,7 @@ export function ProjectsSection() {
     "Farmers can share trucks with other farmers using this platform, reducing transportation costs and improving logistics efficiency.",
   image: "/image1.png",
   github: "https://github.com/dayakar15/Farmers-Logistic-Truck-Sharing",
-  demo: "https://dayakaryeliganti-up7d.vercel.app/",
+  demo: "https://farmerlogictictrucksharing.netlify.app/",
   tech: ["Python", "HTML", "CSS", "Django"],
 },
     {
